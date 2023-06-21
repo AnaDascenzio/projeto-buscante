6c7625c96439e34b8e8128c78f9da473c639aa5b
Ana Nicolly BF Dascenzio <ananicolly.dascenzio@shift.com.br>
2023-06-21 10:45:53 -0300

    service livro

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { LivroService } from '../../service/livro.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy{
 
   listaLivros: any[] = [];
 
-  constructor() { }
+  campoBusca: string = '';
+  subscription: Subscription;
+
+  constructor(
+    private service: LivroService
+  ) { }
+ 
+
+  buscarLivros(){
+    this.subscription = this.service.buscar(this.campoBusca).subscribe(
+     {
+      next: retornoAPI => console.log(retornoAPI),
+      error: erro => console.error(erro),
+      complete: ()=> console.log('Observable completado')
+    }
+    )
+  
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
 }
 
