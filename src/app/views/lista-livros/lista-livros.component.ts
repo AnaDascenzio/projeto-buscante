d9be1f6569401af284f3746aef50e26b381885bd
Ana Nicolly BF Dascenzio <ananicolly.dascenzio@shift.com.br>
2023-06-22 10:47:53 -0300

    inserção de pipes

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,46 +1,47 @@
 import { Component, OnDestroy } from '@angular/core';
 import { LivroService } from '../../service/livro.service';
-import { Subscription } from 'rxjs';
+import { Subscription, catchError, debounceTime, filter, map, switchMap, tap, throwError } from 'rxjs';
 import { Item, Livro } from '../../models/interfaces';
 import { LivroVolumeInfo } from '../../models/livroVolumeInfo';
+import { FormControl } from '@angular/forms';
+
+const PAUSA = 300;
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy{
+export class ListaLivrosComponent{
 
-  listaLivros: Livro[] = [];
 
-  campoBusca: string = '';
-  subscription!: Subscription;
-  livro!: Livro;
+  campoBusca:  FormControl = new FormControl();
+  mensagemErro = '';
 
   constructor(
     private service: LivroService
   ) { }
  
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado)=> valorDigitado.length >= 3 ),
+    tap(()=> console.log('fluxo inicial')),
+    switchMap((valorDigitado)=> this.service.buscar(valorDigitado)),
+    tap(()=> console.log('requisição ao servidor')),
+    map(items =>  this.livrosResultadoParaLivros(items)),
+    catchError(erro =>{
+      console.log(erro)
+      return throwError(()=> new Error(
+        this.mensagemErro = 'Ops, ocorreu um erro...'
+        ))
+    })
+  )
 
-  buscarLivros(){
-    this.subscription = this.service.buscar(this.campoBusca).subscribe(
-     {
-      next: (items) => {
-        this.listaLivros = this.livrosResultadoParaLivros(items)
-      },
-      error: erro => console.error(erro),
-    }
-    )
-  }
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[]{
     return items.map(item => {
       return new LivroVolumeInfo(item);
     })
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 
 }
